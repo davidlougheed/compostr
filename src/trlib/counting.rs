c50ce846fb0765de137b3000ca0ac839e5a5f2f0
David Lougheed <david.lougheed@gmail.com>
2026-03-17 22:31:50 -0400

    named interval struct + n-w traceback fix + motif cigar work

--- src/trlib/counting.rs
+++ src/trlib/counting.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::str::{self, Utf8Error};
 use parasail_rs::prelude::{Aligner, Alignment, Error, Matrix, Table};
 
@@ -26,74 +27,23 @@ pub struct MotifSequenceDecomposer {
     // set of 'canonical' motifs for decomposition:
     pub motif_set: MotifSet,
     // alignment:  TODO: more advanced scoring/matrix
+    match_score: i32,
+    mismatch_score: i32,
+    gap_penalty: i32,
     aligner: Aligner,
     // interval computation parameters
     motif_alignment_score_cutoff: Option<i32>,
 }
 
-/// Given a motif-sequence alignment table and an ending row/col for an alignment, trace back the alignment to return
-/// an interval tuple of: (starting sequence position, ending sequence position, score)
-fn get_interval_from_score_matrix_start_pos(
-    tbl: &Table, mut row: usize, end_col: usize, cutoff: i32
-) -> Option<(usize, usize, i32)> {
-    let mut col = end_col;
-
-    let score = tbl.get(row, col);
-
-    if let Some(s) = score && s >= cutoff {
-        // TODO: keep alignment of motif to sequence from traceback as well.
-        while row > 0 {
-            let mut options: Vec<(usize, usize, i32)> = Vec::new();
-            if col > 0 {
-                if let Some(left) = tbl.get(row, col - 1) { options.push((row, col - 1, left)); }
-                if let Some(diag) = tbl.get(row - 1, col - 1) { options.push((row - 1, col - 1, diag)); }
-            }
-            if let Some(up) = tbl.get(row - 1, col) { options.push((row - 1, col, up)); }
-            let maxopt = options.iter().reduce(|acc, opt| if opt.2 > acc.2 { opt } else { acc } );
-
-            // maxopt shouldn't ever actually be None, otherwise something went wrong with score retrieval somehow.
-            if let Some(&mo) = maxopt {
-                row = mo.0;
-                col = mo.1;
-            } else {
-                return None  // Soemthing went wrong with score retrieval, this shouldn't happen
-            }
-        }
-
-        return Some((col, end_col, s));
-    }
-
-    None
-}
-
 /// Representation of a motif alignment to a sequence.
 /// Format: start (inclusive 0-based), end (inclusive 0-based), score, alignment table index
-#[derive(Debug)]
-#[derive(Clone)]
-struct MotifAlignmentInterval(usize, usize, i32, usize);
-
-/// Given a set of motif alignments (with scoring tables) from Parasail plus an optional scoring cutoff, this function
-/// creates a vector of possible motif alignment intervals in the sequence. These will then be "scheduled" to produce
-/// the sequence motif decomposition.
-fn compute_intervals(
-    alignments: &[(&[u8], Alignment)],
-    motif_alignment_score_cutoff: Option<i32>,
-    seq_len: usize,
-) -> Result<Vec<MotifAlignmentInterval>, Error> {
-    let mut intervals: Vec<MotifAlignmentInterval> = Vec::with_capacity(alignments.len());
-    let cutoff = motif_alignment_score_cutoff.unwrap_or(i32::MIN);
-    for (ai, f) in alignments.iter().enumerate() {
-        let motif_size = f.0.len() - 1;
-        let tbl = f.1.get_score_table()?;
-        eprintln!("{}", tbl);
-
-        for i in 0..seq_len {
-            if let Some(iv) = get_interval_from_score_matrix_start_pos(&tbl, motif_size, i, cutoff) {
-                intervals.push(MotifAlignmentInterval(iv.0, iv.1, iv.2, ai));
-            }
-        }
-    }
-    Ok(intervals)
+#[derive(Clone, Debug)]
+pub struct MotifAlignmentInterval {
+    start: usize,
+    end: usize,
+    score: i32,
+    cigar: String,
+    motif_idx: usize,
 }
 
 fn backtrack_schedule(
@@ -106,7 +56,7 @@ fn backtrack_schedule(
     if j == 0 {
         return;
     }
-    if intervals[j-1].2 + m[p[j]] >= m[j - 1] {
+    if intervals[j-1].score + m[p[j]] >= m[j - 1] {
         let temp: MotifAlignmentInterval = intervals[j-1].clone();
         final_schedule.push(temp);
         backtrack_schedule(final_schedule, intervals, m, p, p[j])
@@ -123,16 +73,16 @@ fn schedule(
     intervals: &[MotifAlignmentInterval], // Vector of tuples (start, end, score, alignment index)
 ) -> (Vec<MotifAlignmentInterval>, i32) {
 
-    //sort intervals globally by earliest to latest end index
+    // sort intervals globally by earliest to latest end index
     let mut s_intervals: Vec<&MotifAlignmentInterval> = intervals.iter().collect();
-    s_intervals.sort_by(|x, y| x.1.cmp(&y.1));
+    s_intervals.sort_by(|x, y| x.end.cmp(&y.end));
 
     // p[j] is the index of the latest interval that ends before interval j begins
     let mut p = vec![0; s_intervals.len()+1];
     for i in 1..s_intervals.len() + 1 {
         let mut n = i - 1;
         while n > 0 {
-            if s_intervals[n].1 < s_intervals[i-1].0 {
+            if s_intervals[n].end < s_intervals[i-1].start { // interval coordinates are inclusive, so use '<'
                 p[i] = n + 1;
                 break;
             }
@@ -160,6 +110,40 @@ fn schedule(
     (final_schedule, m[s_intervals.len()])
 }
 
+/// Encodes a CIGAR-ish alignment operation (insertion/deletion/match/mismatch).
+/// With a grouping function, this can become a real CIGAR.
+#[derive(Clone, Debug, PartialEq)]
+enum AlignmentItem {
+    Ins,
+    Del,
+    Match,
+    Mismatch,
+}
+
+fn alignment_items_to_cigar(items: &[AlignmentItem]) -> String {
+    let mut cigar = String::new();
+    let mut ii = items.iter();
+    if let Some(first) = ii.next() {
+        let mut current_op = first;
+        let mut current_count: usize = 1;
+        for op in ii {
+            if op == current_op {
+                current_count += 1;
+            } else {
+                cigar = format!("{}{}{}", cigar, current_count, match current_op {
+                    AlignmentItem::Ins => "I",
+                    AlignmentItem::Del => "D",
+                    AlignmentItem::Match => "=",
+                    AlignmentItem::Mismatch => "X",
+                });
+                current_op = op;
+                current_count = 0;
+            }
+        }
+    }
+    cigar
+}
+
 impl MotifSequenceDecomposer {
     pub fn new(
         motif_set: MotifSet,
@@ -174,11 +158,97 @@ impl MotifSequenceDecomposer {
             .gap_open(gap_penalty)
             .gap_extend(gap_penalty)
             .semi_global()
+            .allow_ref_gaps(vec![String::from("prefix"), String::from("suffix")])
             .use_table()
             .striped()
             .build();
 
-        MotifSequenceDecomposer { motif_set, aligner, motif_alignment_score_cutoff }
+        MotifSequenceDecomposer {
+            motif_set, match_score, mismatch_score, gap_penalty, aligner, motif_alignment_score_cutoff
+        }
+    }
+
+    /// Given a motif-sequence alignment table and an ending row/col for an alignment, trace back the alignment to return
+    /// an interval tuple of: (starting sequence position, ending sequence position, score)
+    fn get_interval_from_score_matrix_start_pos(
+        &self, seq: &[u8], motif: &[u8], tbl: &Table, mut row: usize, end_col: usize, cutoff: i32
+    ) -> Option<(usize, usize, i32, String)> {
+        let mut col = end_col;
+
+        let score = tbl.get(row, col);
+
+        if let Some(s) = score && s >= cutoff {
+            // keep alignment of motif to sequence from traceback as well:
+            let mut motif_alignment: Vec<AlignmentItem> = Vec::new();
+
+            while row > 0 {
+                let mut options: Vec<(usize, usize, i32, AlignmentItem)> = Vec::new();
+                if col > 0 {
+                    // TODO: this doesn't support affine gap properly
+                    if let Some(left) = tbl.get(row, col - 1) {
+                        options.push((row, col - 1, left - self.gap_penalty, AlignmentItem::Ins));
+                    }
+                    if let Some(diag) = tbl.get(row - 1, col - 1) {
+                        let (sc, ait) = if motif[row - 1] == seq[col - 1] {
+                            (self.match_score, AlignmentItem::Match)
+                        } else {
+                            (self.mismatch_score, AlignmentItem::Mismatch)
+                        };
+                        options.push((row - 1, col - 1, diag + sc, ait));
+                    }
+                }
+                // TODO: this doesn't support affine gap properly
+                if let Some(up) = tbl.get(row - 1, col) { options.push((row - 1, col, up  - self.gap_penalty, AlignmentItem::Del)); }
+                let maxopt = options.iter().reduce(|acc, opt| if opt.2 > acc.2 { opt } else { acc } );
+
+                // maxopt shouldn't ever actually be None, otherwise something went wrong with score retrieval somehow.
+                if let Some(mo) = maxopt {
+                    row = mo.0;
+                    col = mo.1;
+                    motif_alignment.push(mo.3.clone());
+                } else {
+                    return None  // Something went wrong with score retrieval, this shouldn't happen
+                }
+            }
+
+            motif_alignment.reverse();
+            let cigar = alignment_items_to_cigar(&motif_alignment);
+
+            return Some((col, end_col, s, cigar));
+        }
+
+        None
+    }
+
+    /// Given a set of motif alignments (with scoring tables) from Parasail plus an optional scoring cutoff, this function
+    /// creates a vector of possible motif alignment intervals in the sequence. These will then be "scheduled" to produce
+    /// the sequence motif decomposition.
+    fn compute_intervals(
+        &self,
+        seq: &[u8],
+        alignments: &[(&[u8], Alignment)],
+        motif_alignment_score_cutoff: Option<i32>,
+        seq_len: usize,
+    ) -> Result<Vec<MotifAlignmentInterval>, Error> {
+        let mut intervals: Vec<MotifAlignmentInterval> = Vec::with_capacity(alignments.len());
+        let cutoff = motif_alignment_score_cutoff.unwrap_or(i32::MIN);
+        for (ai, f) in alignments.iter().enumerate() {
+            let motif_size = f.0.len() - 1;
+            let tbl = f.1.get_score_table()?;
+            eprintln!("scr {}", tbl);
+
+            intervals.extend((0..seq_len).filter_map(|i| {
+                self.get_interval_from_score_matrix_start_pos(&seq, &f.0, &tbl, motif_size, i, cutoff)
+                    .map(|iv| MotifAlignmentInterval {
+                        start: iv.0,
+                        end: iv.1,
+                        score: iv.2,
+                        cigar: iv.3,
+                        motif_idx: ai,
+                    })
+            }));
+        }
+        Ok(intervals)
     }
 
     /// Main functionality for MotifSequenceDecomposer - given a sequence, decomposes it into motifs using the motif
@@ -194,7 +264,7 @@ impl MotifSequenceDecomposer {
 
         //  2. determine intervals using some kind of heuristic so we don't have an absurd number?
         //     or just use last row(?) of the matrix as the score + figure out the interval... + do a little trimming
-        let intervals = compute_intervals(&alignments, self.motif_alignment_score_cutoff, seq.len())?;
+        let intervals = self.compute_intervals(&seq, &alignments, self.motif_alignment_score_cutoff, seq.len())?;
         eprintln!("{:?}", intervals);
 
         //  3: use weighted interval scheduling algorithm https://en.wikipedia.org/wiki/Interval_scheduling#Weighted
@@ -207,7 +277,7 @@ impl MotifSequenceDecomposer {
     pub fn decomp_to_str(&self, decomp: MotifSequenceDecomposition) -> Result<Vec<&str>, Utf8Error> {
         let mut return_vec = Vec::new();
         for i in decomp.decomposition.iter() {
-            let decomp_str: &str = str::from_utf8(self.motif_set.motifs[i.3].as_slice())?;
+            let decomp_str: &str = str::from_utf8(self.motif_set.motifs[i.motif_idx].as_slice())?;
             return_vec.push(decomp_str);
         }
         Ok(return_vec)
@@ -221,6 +291,7 @@ mod tests {
 
     #[rstest]
     #[case(b"CAGCAGCAGCAGCAGCAGCAGCAGCAG".to_vec(), vec!["CAG", "CAG", "CAG", "CAG", "CAG", "CAG", "CAG", "CAG", "CAG"])]
+    #[case(b"CCGCCGCCGCCGCCGCCGCCGCCGCCG".to_vec(), vec!["CCG", "CCG", "CCG", "CCG", "CCG", "CCG", "CCG", "CCG", "CCG"])]
     #[case(b"CAGCAGCGGCAGCAAG".to_vec(), vec!["CAG", "CAG", "CGG", "CAG", "CAAG"])]
     #[case(b"CAGCAGCAAGTTCAGCCGCCGCCCG".to_vec(), vec!["CAG", "CAG", "CAAG", "T", "T", "CAG", "CCG", "CCG", "CCCG"])]
     fn test_decomposition(#[case] seq: Vec<u8>, #[case] expected_decomp: Vec<&str>) {
