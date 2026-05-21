f7e2326ebbccb45b4a64ccfc7fb5ddad68ba77f2
Aidon Lebar <aidonlebar@gmail.com>
2026-05-21 11:50:17 -0400

    Fix indexing

--- trlib/src/nw.rs
+++ trlib/src/nw.rs
@@ -36,10 +36,10 @@ impl Aligner {
         let motif_len = motif.len();
         let mut m_score: Array2<i32> = Array::zeros((seq_len + 1, motif_len + 1));
         for i in 1..seq_len {
-             m_score[i][0] = -(i as i32);
+             m_score[[i,0]] = -(i as i32);
         }
         for i in 1..motif_len{
-            m_score[0][i] = -(i as i32);
+            m_score[[0,i]] = -(i as i32);
         }
 
         let mut m_trace: Array2<TraceItem> = Array::from_elem((seq.len() + 1, motif.len() + 1), TraceItem::Unset);
@@ -51,33 +51,33 @@ impl Aligner {
             //iterate the diagonal
             while y <= motif_len && x > 0 {
                 //up
-                let up = if m_trace[x][y-1] == TraceItem::Up || m_trace[x][y-1] == TraceItem::Left {
-                    m_score[x][y-1] + self.gap_extend
+                let up = if m_trace[[x,y-1]] == TraceItem::Up || m_trace[[x,y-1]] == TraceItem::Left {
+                    m_score[[x,y-1]] + self.gap_extend
                 } else {
-                    m_score[x][y-1] + self.gap_open
+                    m_score[[x,y-1]] + self.gap_open
                 };
                 //left
-                let left = if m_trace[x-1][y] == TraceItem::Up || m_trace[x-1][y] == TraceItem::Left {
-                    m_score[x-1][y] + self.gap_extend
+                let left = if m_trace[[x-1,y]] == TraceItem::Up || m_trace[[x-1,y]] == TraceItem::Left {
+                    m_score[[x-1,y]] + self.gap_extend
                 } else {
-                    m_score[x-1][y] + self.gap_open
+                    m_score[[x-1,y]] + self.gap_open
                 };
                 //(mis)match - upleft
                 let upleft = if motif[y] == seq[x] {
-                    m_score[x-1][y -1] + self.match_score
+                    m_score[[x-1,y-1]] + self.match_score
                 } else {
-                    m_score[x-1][y -1] + self.mismatch_score;
+                    m_score[[x-1,y-1]] + self.mismatch_score
                 };
 
                 if up > upleft && up > left {
-                    m_score[x][y] = up;
-                    m_trace[x][y] = TraceItem::Up;
+                    m_score[[x,y]] = up;
+                    m_trace[[x,y]] = TraceItem::Up;
                 } else if left > upleft && left > up {
-                    m_score[x][y] = left;
-                    m_trace[x][y] = TraceItem::Left;
+                    m_score[[x,y]] = left;
+                    m_trace[[x,y]] = TraceItem::Left;
                 } else {
-                    m_score[x][y] = upleft;
-                    m_trace[x][y] = TraceItem::Diag;
+                    m_score[[x,y]] = upleft;
+                    m_trace[[x,y]] = TraceItem::Diag;
                 }
                 x -= 1;
                 y += 1;
